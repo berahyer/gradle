4d89db9151d365ca1fc3909549a79cf060393500
nogah <nogah@bliex.com>
2017-01-11 21:04:57 +0900

    first commit

---
+++ sd-front-web/src/main/java/com/samsung/SDFront.java
@@ -0,0 +1,10 @@
+package com.samsung;
+
+import com.samsung.util.Library;
+
+public class SDFront{
+	public static void main(String... args) { 
+		System.out.println(Integer.toString(Library.sumValue(1,3)));
+	} 
+}
+
